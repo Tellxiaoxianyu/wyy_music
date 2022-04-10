b84f8448efc395fe75ae9cd33cabe0ec9d63ea8e
Tellxiaoxianyu <1048952317@qq.com>
2022-04-10 21:21:04 +0800

    发现页面完善

---
+++ src/components/content/discoverMusic/SongList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      SongList
+  </div>
+</template>
+
+<script>
+export default {
+    name:"SongList",
+    data(){
+    return {
+      activeIndex : '3',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
